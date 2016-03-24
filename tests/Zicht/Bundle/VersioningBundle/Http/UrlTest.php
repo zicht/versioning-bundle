d6d9b9174b955607b83d35ab826a1c1dd05af089
Gerard van Helden <gerard@zicht.nl>
2016-03-24 11:06:12 +0000

    added test case for active version
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtVersioningBundle/trunk@81681 0f35748b-8228-47f0-bbea-889afbbd9a42

--- tests/Zicht/Bundle/VersioningBundle/Http/UrlTest.php
+++ tests/Zicht/Bundle/VersioningBundle/Http/UrlTest.php
@@ -6,20 +6,48 @@
 
 namespace Zicht\Bundle\VersioningBundle\Http;
 
+use Symfony\Component\HttpFoundation\Request;
 use Zicht\Bundle\RcoSiteBundle\Entity\Page\HomePage;
 use Zicht\Bundle\VersioningBundle\Entity\EntityVersion;
 
-class UrlTest extends \PHPUnit_Framework_TestCase
+class UrlHelperTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAddUrlParams()
+    public function testUrlHelper()
     {
         $helper = new UrlHelper();
 
         $v = new EntityVersion();
+        $v->setIsActive(false);
         $v->setVersionNumber(5678);
         $v->setOriginalId(1234);
         $v->setSourceClass(HomePage::class);
 
-        var_dump($helper->decorateVersionUrl('/', $v));
+        $decorated = $helper->decorateVersionUrl('http://example.org/url?param1=param2', $v);
+
+        $query = parse_url($decorated, PHP_URL_QUERY);
+        $params = [];
+        parse_str($query, $params);
+        $request = new Request($params);
+
+        $versionInfo = $helper->extractVersionInfo($request);
+
+        $this->assertArrayHasKey(HomePage::class, $versionInfo);
+        $this->assertArrayHasKey(1234, $versionInfo[HomePage::class]);
+        $this->assertEquals(5678, $versionInfo[HomePage::class][1234]);
+    }
+
+
+    public function testUrlHelperIgnoresVersionIdIfIsActive()
+    {
+        $helper = new UrlHelper();
+
+        $v = new EntityVersion();
+        $v->setIsActive(true);
+        $v->setVersionNumber(5678);
+        $v->setOriginalId(1234);
+        $v->setSourceClass(HomePage::class);
+
+        $decorated = $helper->decorateVersionUrl('http://example.org/url?param1=param2', $v);
+        $this->assertEquals('http://example.org/url?param1=param2', $decorated);
     }
 }
