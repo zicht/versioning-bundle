ca5afcd9e47fa80da3430e9426f86457c360b7b2
Gerard van Helden <gerard@zicht.nl>
2016-03-25 10:03:14 +0000

    doc
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtVersioningBundle/trunk@81763 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/VersioningBundle/Model/EntityVersionStorageInterface.php
+++ src/Zicht/Bundle/VersioningBundle/Model/EntityVersionStorageInterface.php
@@ -6,7 +6,41 @@
 
 namespace Zicht\Bundle\VersioningBundle\Model;
 
+/**
+ * Base interface for storing entity versions
+ */
 interface EntityVersionStorageInterface
 {
+    /**
+     * Find all versions for the specified object.
+     *
+     * @param VersionableInterface $object
+     * @return EntityVersionInterface[]
+     */
     public function findVersions(VersionableInterface $object);
+
+    /**
+     * Find the specified version of the specified object
+     *
+     * @param VersionableInterface $entity
+     * @param int $versionId
+     * @return EntityVersionInterface
+     */
+    public function findVersion(VersionableInterface $entity, $versionId);
+
+    /**
+     * Find the active version for the specified entity.
+     *
+     * @param VersionableInterface $entity
+     * @return mixed
+     */
+    public function findActiveEntityVersion(VersionableInterface $entity);
+
+    /**
+     * Get the next version number for the specified entity.
+     *
+     * @param VersionableInterface $entity
+     * @return mixed
+     */
+    public function getNextVersionNumber(VersionableInterface $entity);
 }
