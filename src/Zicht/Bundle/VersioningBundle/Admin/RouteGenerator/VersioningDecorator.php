360d74818650fb7354c9a339b81abf4c87ad1663
Gerard van Helden <gerard@zicht.nl>
2016-03-30 16:42:32 +0000

    fixes redirect url after edit
    git-svn-id: svn://zon/projecten/zicht-libraries/ZichtVersioningBundle/trunk@81984 0f35748b-8228-47f0-bbea-889afbbd9a42

--- src/Zicht/Bundle/VersioningBundle/Admin/RouteGenerator/VersioningDecorator.php
+++ src/Zicht/Bundle/VersioningBundle/Admin/RouteGenerator/VersioningDecorator.php
@@ -11,6 +11,7 @@ use Sonata\AdminBundle\Route\RouteGeneratorInterface;
 use Zicht\Bundle\RcoSiteBundle\Admin\ContentItem\ContentItemDetailAdmin;
 use Zicht\Bundle\VersioningBundle\Http\UrlHelper;
 use Zicht\Bundle\VersioningBundle\Manager\VersioningManager;
+use Zicht\Bundle\VersioningBundle\Model\VersionableInterface;
 
 class VersioningDecorator implements RouteGeneratorInterface
 {
@@ -24,7 +25,10 @@ class VersioningDecorator implements RouteGeneratorInterface
     public function generateUrl(AdminInterface $admin, $name, array $parameters = array(), $absolute = false)
     {
         $url = $this->generator->generateUrl($admin, $name, $parameters, $absolute);
-        if ($admin instanceof ContentItemDetailAdmin && $name === 'edit') {
+        if (
+            ($admin instanceof ContentItemDetailAdmin && in_array($name, ['edit', 'list']))
+            || ((new \ReflectionClass($admin->getClass()))->implementsInterface(VersionableInterface::class) && in_array($name, ['show', 'edit']))
+        ) {
             $url = $this->urlHelper->decorateVersionsUrl(
                 $url,
                 $this->versioning->getLoadedVersions()
