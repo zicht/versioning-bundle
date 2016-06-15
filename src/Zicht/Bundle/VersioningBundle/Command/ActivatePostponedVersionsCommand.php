d679b240c14cfdde63edfa621d0501ecc1ca97c9
Gerard van Helden <gerard@zicht.nl>
2016-06-15 21:49:20 +0200

    To make the collections work, the activate command must use the same api for activating versions

--- src/Zicht/Bundle/VersioningBundle/Command/ActivatePostponedVersionsCommand.php
+++ src/Zicht/Bundle/VersioningBundle/Command/ActivatePostponedVersionsCommand.php
@@ -10,10 +10,12 @@ use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher;
 use Zicht\Bundle\VersioningBundle\Event\VersioningEvent;
 use Zicht\Bundle\VersioningBundle\Manager\VersioningManager;
+use Zicht\Bundle\VersioningBundle\Model\VersionableInterface;
 use Zicht\Itertools as iter;
 
 /**
@@ -53,7 +55,9 @@ class ActivatePostponedVersionsCommand extends ContainerAwareCommand
     {
         $this
             ->setName('zicht:versioning:schedule')
-            ->setDescription('Search for versions that are supposed to be activated');
+            ->addOption('dry-run', '', InputOption::VALUE_NONE, 'Do a dry run (don\'t commit changes)')
+            ->setDescription('Search for versions that are supposed to be activated')
+        ;
     }
 
     /**
@@ -61,57 +65,59 @@ class ActivatePostponedVersionsCommand extends ContainerAwareCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        /** EventDispatcher */
-        $dispatcher = $this->getContainer()->get('event_dispatcher');
-
         $this->versioning->setSystemToken();
-
-        $unpublishRecords = 'UPDATE _entity_version SET is_active = false WHERE original_id = :original_id';
-        $makeRecordActive = 'UPDATE _entity_version SET is_active = true WHERE id = :id LIMIT 1';
+        $isDryRun = $input->getOption('dry-run');
 
         /** @var Connection $connection */
         $connection = $this->doctrine->getConnection();
         $nowDate = new \DateTime('now');
         $now = $nowDate->format('Y-m-d H:i:s');
 
-        $stmt = $connection->prepare('SELECT id, version_number, date_active_from, source_class, original_id, is_active FROM _entity_version WHERE date_active_from <= :date_active_from ORDER BY date_active_from DESC LIMIT 1');
+        $stmt = $connection->prepare('SELECT id, version_number, date_active_from, source_class, original_id, is_active, based_on_version FROM _entity_version WHERE date_active_from <= :date_active_from ORDER BY date_active_from DESC LIMIT 1');
         $stmt->execute([':date_active_from' => $now]);
 
         $result = $stmt->fetchAll();
         $result = iter\groupby('original_id', $result);
 
         $connection->beginTransaction();
-
+        $i = 0;
         foreach ($result as $entityId => $records) {
             // First record is the one that should be published, we are not interested in the rest
             $scheduledVersion = $records[0];
 
             if ($scheduledVersion['is_active']) {
-                // The latest scheduled entity is the one that should be active already so skip.
+                // The latest scheduled entity is the one that is being activated right now, so if it's already active,
+                // nothing to be done here.
                 continue;
             }
+            $i ++;
 
-            $unpublishRecordsStmt = $connection->prepare($unpublishRecords);
-            $unpublishRecordsStmt->bindValue(':original_id', $scheduledVersion['original_id']);
-            $unpublishRecordsStmt->execute();
+            $this->versioning->setVersionToLoad($scheduledVersion['source_class'], $scheduledVersion['original_id'], $scheduledVersion['version_number']);;
+            /** @var VersionableInterface $entity */
+            $entity = $this->doctrine->getManager()->find($scheduledVersion['source_class'], $scheduledVersion['original_id']);
+            $this->versioning->setVersionOperation($entity, VersioningManager::VERSION_OPERATION_ACTIVATE, $scheduledVersion['version_number'], $scheduledVersion['based_on_version']);
+            $this->doctrine->getManager()->persist($entity);
 
-            // Set this record to active
-            $makeActiveStmt = $connection->prepare($makeRecordActive);
-            $makeActiveStmt->bindValue(':id', $scheduledVersion['id']);
-            $makeActiveStmt->execute();
-            $output->writeln(sprintf('<comment>Version Id: %d is set to active (OriginalId: %d)</comment>', $scheduledVersion['id'], $scheduledVersion['original_id']));
             $this->getContainer()->get('logger')->info(
                 sprintf(
-                    'Activated version %d of entity %s@%d (scheduled time)',
+                    'Activated version %d of entity %s@%d (scheduled time) %s',
                     $scheduledVersion['version_number'],
                     $scheduledVersion['source_class'],
-                    $scheduledVersion['original_id']
+                    $scheduledVersion['original_id'],
+                    $isDryRun ? ' [DRY RUN]' : ''
                 )
             );
 
-            $dispatcher->dispatch('zicht_versioning.activated', new VersioningEvent($scheduledVersion['id'], $scheduledVersion['original_id']));
+            $output->writeln(sprintf('<comment>Version Id: %d is set to active (OriginalId: %d) %s</comment>', $scheduledVersion['id'], $scheduledVersion['original_id'], $isDryRun ? ' [DRY RUN]' : ''));
         }
 
-        $connection->commit();
+        if ($i > 0) {
+            if ($isDryRun) {
+                $connection->rollBack();
+            } else {
+                $this->doctrine->getManager()->flush();
+                $connection->commit();
+            }
+        }
     }
 }
